b16190792086c32f643a16bfad043cf15cfff9e1
Leonardo Lima <83369558+leolimag@users.noreply.github.com>
2022-12-02 10:49:28 -0300

    Update success.php

--- two-factor-authentication/success.php
+++ two-factor-authentication/success.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Código correto</title>
+    <title>CÃģdigo correto</title>
 </head>
 <body class="background">
 <div class="content-success">
-    <h1>Código inserido corretamente!</h1>
+    <h1>CÃģdigo inserido corretamente!</h1>
     <p><a class="link" href="index.php">Voltar</a></p>
 </div>
 </body>
