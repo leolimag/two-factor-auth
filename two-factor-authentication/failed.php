3405124d59bbfcfd929fc36ae96a03fd1f09adb5
Leonardo Lima <83369558+leolimag@users.noreply.github.com>
2022-12-02 10:47:58 -0300

    Update failed.php

--- two-factor-authentication/failed.php
+++ two-factor-authentication/failed.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Código incorreto</title>
+    <title>CÃģdigo incorreto</title>
 </head>
 <body class="background">
 <div class="content-success">
-    <h1>Código incorreto. Tente Novamente.</h1>
+    <h1>CÃģdigo incorreto. Tente Novamente.</h1>
     <p><a class="link" href="index.php">Voltar</a></p>
 </div>
 </body>
